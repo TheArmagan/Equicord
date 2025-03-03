dc08c1f3d6ad449a5e00248b6dc9f56add394493
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2025-03-03 16:44:43 +0300

    Implement FollowVoiceUser plugin.
    FollowVoiceUser allows you to follow any user in voice chat if they are same in same guilds as you.

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1008,6 +1008,10 @@ export const EquicordDevs = Object.freeze({
         name: "port",
         id: 1318383159645311009n,
     },
+    TheArmagan: {
+        name: "TheArmagan",
+        id: 707309693449535599n
+    }
 } satisfies Record<string, Dev>);
 
 // iife so #__PURE__ works correctly
