c1c90c654e939f267e51aeb9ee55bdd1ffabaff6
thororen <78185467+thororen1234@users.noreply.github.com>
2025-03-04 14:48:54 -0500

    Patch Call Menu For Ignore Button

--- src/equicordplugins/ignoreCalls/index.tsx
+++ src/equicordplugins/ignoreCalls/index.tsx
@@ -5,8 +5,10 @@
  */
 
 import { NavContextMenuPatchCallback } from "@api/ContextMenu";
+import { definePluginSettings } from "@api/Settings";
+import { ErrorBoundary } from "@components/index";
 import { EquicordDevs } from "@utils/constants";
-import definePlugin from "@utils/types";
+import definePlugin, { OptionType } from "@utils/types";
 import { FluxDispatcher, Menu, React, UserStore } from "@webpack/common";
 import { Channel } from "discord-types/general";
 
@@ -36,26 +38,57 @@ const ContextMenuPatch: NavContextMenuPatchCallback = (children, { channel }: { 
     );
 };
 
+const settings = definePluginSettings({
+    ignoreTimeout: {
+        type: OptionType.SLIDER,
+        description: "Timeout to click ignore",
+        markers: [0, 1000, 2500, 5000, 10000, 20000],
+        default: 2500,
+        stickToMarkers: true,
+    }
+});
+
 
 export default definePlugin({
     name: "IgnoreCalls",
     description: "Allows you to ignore calls from specific users or dm groups.",
     authors: [EquicordDevs.TheArmagan],
-    flux: {
-        async CALL_UPDATE({ channelId, ringing, messageId, region }: { channelId: string; ringing: string[]; messageId: string; region: string; }) {
-            if (!ignoredChannelIds.has(channelId)) return;
-            const currentUserId = UserStore.getCurrentUser().id;
-
-            if (ringing.includes(currentUserId)) {
-                return FluxDispatcher.dispatch({
-                    type: "CALL_UPDATE",
-                    channelId,
-                    ringing: ringing.filter((id: string) => id !== currentUserId),
-                    messageId,
-                    region
-                });
+    patches: [
+        {
+            find: "#{intl::INCOMING_CALL_ELLIPSIS}",
+            replacement: {
+                match: /(?<=channel:(\i).{0,50}INCOMING_CALL_MODAL\).*?)(\}\)\]\}\))\]/,
+                replace: "$2,$self.renderIgnore($1)]"
             }
         }
+    ],
+    settings,
+    flux: {
+        async CALL_UPDATE({ channelId, ringing, messageId, region }: { channelId: string; ringing: string[]; messageId: string; region: string; }) {
+            setTimeout(() => {
+                if (!ignoredChannelIds.has(channelId)) return;
+                const currentUserId = UserStore.getCurrentUser().id;
+                if (ringing.includes(currentUserId)) {
+                    return FluxDispatcher.dispatch({
+                        type: "CALL_UPDATE",
+                        channelId,
+                        ringing: ringing.filter((id: string) => id !== currentUserId),
+                        messageId,
+                        region
+                    });
+                }
+            }, settings.store.ignoreTimeout);
+        }
+    },
+    renderIgnore(channel) {
+        const handeClick = () => {
+            ignoredChannelIds.add(channel.id);
+        };
+        return (
+            <ErrorBoundary>
+                <span onClick={handeClick} style={{ textAlign: "center", color: "var(--text-danger)" }}>Ignore</span>
+            </ErrorBoundary >
+        );
     },
     contextMenus: {
         "user-context": ContextMenuPatch,
